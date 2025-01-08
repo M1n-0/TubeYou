c1b7547330143c72a593d1096b2410cfec10390b
M1n-0 <145598371+M1n-0@users.noreply.github.com>
2025-01-08 10:13:43 +0100

    modif port

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = 4000;
 
 // Dossier où sont stockées les vidéos
 const videoDir = path.join(__dirname, 'video');
@@ -12,6 +12,17 @@ const videoDir = path.join(__dirname, 'video');
 app.use('/video', express.static(videoDir));
 app.use(express.static('public')); // Pour servir votre fichier HTML
 
+// Charge les fichiers du dossier assets.
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
+
+// Charge les fichiers html du dossier html
+app.use('/html', express.static(path.join(__dirname, 'html')));
+
+// charge le fichier index.html comme page root
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
 // Route pour récupérer la liste des vidéos
 app.get('/api/videos', (req, res) => {
     fs.readdir(videoDir, (err, files) => {
@@ -19,14 +30,13 @@ app.get('/api/videos', (req, res) => {
             return res.status(500).json({ error: 'Erreur lors de la lecture du dossier' });
         }
 
-        // Filtrer les fichiers pour n'obtenir que les vidéos
         const videos = files.filter(file => {
             return file.endsWith('.mp4') || file.endsWith('.webm') || file.endsWith('.ogg');
         }).map(file => {
             return {
                 name: path.parse(file).name,
                 src: '/video/' + file,
-                poster: '/video/' + path.parse(file).name + '.jpg' // Assumer que le poster a le même nom
+                poster: '/video/' + path.parse(file).name + '.jpg'
             };
         });
 
@@ -34,17 +44,6 @@ app.get('/api/videos', (req, res) => {
     });
 });
 
-// Serve static files from the "assets" directory
-app.use('/assets', express.static(path.join(__dirname, 'assets')));
-
-// Serve HTML files from the "html" directory
-app.use('/html', express.static(path.join(__dirname, 'html')));
-
-// Serve the index.html file at the root URL
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-});
-
 app.listen(port, '0.0.0.0', () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`TubeYou listening at http://localhost:${port}`);
 });
